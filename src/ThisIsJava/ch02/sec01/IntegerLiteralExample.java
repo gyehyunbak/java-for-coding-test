81355c1d2452b2165f18c00859ccda197fc1fb76
Gyehyun Park <gyehyunbak@gmail.com>
2023-05-04 13:19:40 +0900

    20230504-2

---
+++ src/ThisIsJava/ch02/sec01/IntegerLiteralExample.java
@@ -0,0 +1,13 @@
+package ThisIsJava.ch02.sec01;
+
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+        // 리터럴: 코드에서 프로그래머가 직접 입력한 값
+        int var1 = 0b1011;  // 2진수
+        int var2 = 0206;    // 8진수
+        int var3 = 365;     // 10진수
+        int var4 = 0xB3;    // 16진수
+
+        System.out.println();
+    }
+}
