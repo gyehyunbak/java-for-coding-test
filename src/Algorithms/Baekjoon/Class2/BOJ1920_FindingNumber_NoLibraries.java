d812c3954ad5e1d3755e3c5b3e4e90df238d42b4
Gyehyun Park <gyehyunbak@gmail.com>
2023-05-11 22:44:38 +0900

    20230511_2244

--- src/Algorithms/Baekjoon/Class2/BOJ1920_FindingNumber_NoLibraries.java
+++ src/Algorithms/Baekjoon/Class2/BOJ1920_FindingNumber_NoLibraries.java
@@ -3,10 +3,11 @@ package Algorithms.Baekjoon.Class2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ1920_FindingNumber {
+public class BOJ1920_FindingNumber_NoLibraries {
+    public static int[] arr;
+    public static int[] tmp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -14,20 +15,30 @@ public class BOJ1920_FindingNumber {
         int m = Integer.parseInt(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
 
-        int[] arrA = new int[n];
+        arr = new int[n];
 
         for(int i=0; i<n; i++) {
-            arrA[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arrA);
-
         for(int i=0; i<m; i++) {
-            if(Arrays.binarySearch(arrA, Integer.parseInt(st2.nextToken())) >= 0) {
+            if(binarySearch(0, n, Integer.parseInt(st2.nextToken())) != -1) {
                 System.out.println(1);
             } else {
                 System.out.println(0);
             }
+        }
+    }
+
+    public static int binarySearch(int low, int high, int item) {
+        if(high - low < 0) return -1;
+        int mid = (high - low) / 2;
+        if(arr[mid] == item) {
+            return mid;
+        } else if(arr[mid] > item) {
+            return binarySearch(mid+1, high, item);
+        } else {
+            return binarySearch(low, mid-1, item);
         }
     }
 }
